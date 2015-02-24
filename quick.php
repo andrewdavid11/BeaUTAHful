9ca5a45a1536947b1aa0e95ab00440bf37ac57a9
Andrew David <andrewdavidhodgson@gmail.com>
2015-02-24 01:57:08 -0700

    Quick Orders Page updated with Post Card pack options. Cart not updated, nor database.

--- quick.php
+++ quick.php
@@ -25,19 +25,114 @@
     <h1>Quick Ordering</h1>
       <p>This page is best for return customers already familiar with my products and in a hurry. For more details about my products, or to leisurely view individual photos, try the 
       <a href="search.php">Search</a> or <a href="galleries.php">Galleries</a> pages.</p>
+      <h3>4" X 6" Post Cards</h3>
+      <img src="" alt="A variety of postcards laid out."/>
+      <p>I print post cards at home. These are my most economical option for printing. Larger flip cards with a colorful background
+      are listed below.
+      </p>
+      <hr />
+      <h3>10 Post Card Packs</h3>
+        <p>10 Unique 4" X 6" Post Cards from the theme of your choice. Lots of options. Packs are $10.00, for a price of $1.00 per card.<br />
+        All cards unique. (No photo used more than once.)<br />
+        </p>
+	      <form method="post" action="cart.php">
+			<p>
+			  <select name="BuyPostCard10" class="button_left">
+				  <option value="" selected="selected">10 Card Value Packs: $25.00 ($2.50 per card)</option>
+				  <option value="1001 | PostCard10 | Utah">Utah Pack</option>
+				  <option value="1001 | PostCard10 | Colorado">Colorado Pack</option>
+				  <option value="1001 | PostCard10 | Sawtooths">Idaho Sawtooths Pack</option>
+				  <option value="1001 | PostCard10 | Glacier NP">Glacier National Park Pack</option>
+				  <option value="1001 | PostCard10 | Arizona">Arizona Pack</option>
+				  <option value="1001 | PostCard10 | Washington">Washington Pack</option>
+				  <option value="1001 | PostCard10 | Oregon">Oregon Pack</option>
+				  <option value="1001 | PostCard10 | Nevada">Nevada Pack</option>
+				  <option value="1001 | PostCard10 | Wind River">Wind River Pack</option>
+				  <option value="1001 | PostCard10 | Tetons">Teton Pack</option>
+				  <option value="1001 | PostCard10 | Wildlife">Wildlife Pack</option>
+				  <option value="1001 | PostCard10 | Sunset">Sunset Pack</option>
+				  <option value="1001 | PostCard10 | Desert">Desert Pack</option>
+				  <option value="1001 | PostCard10 | Waterfall">Waterfall Pack</option>
+				  <option value="1001 | PostCard10 | Icefall">Icefall Pack</option>
+				  <option value="1001 | PostCard10 | Reflections">Reflections Pack</option>
+				  <option value="1001 | PostCard10 | Wildflower">Wildflower Pack</option>
+				  <option value="1001 | PostCard10 | Tulip">Tulip Pack</option>
+				  <option value="1001 | PostCard10 | Forest">Forest Pack</option>
+				  <option value="1001 | PostCard10 | Autumn">Autumn Pack</option>
+				  <option value="1001 | PostCard10 | Winter">Winter Pack</option>
+				  <option value="1001 | PostCard10 | Summits">Summit View Pack</option>
+				  <option value="1001 | PostCard10 | Sunrise/Sunset">Sunrise/set Pack</option>
+			  </select>&nbsp;
+				<input type="hidden" name="postcard10-submitted" value="yes" /> 
+				<input type="submit" name="submit-postcard10" value="Add to Cart" class="button_right"/>
+			</p>
+		  </form>
+	<h3>25 Post Card Packs</h3>  
+		   <p>25 unique 4" X 6" Post Cards from the category of your choice. These packs are $20 each, or $0.80 per card.
+		   All cards unique. (No photo is used multiple times.)<br />
+		   </p>
+		   <form method="post" action="cart.php">
+			<p>
+			  <select name="BuyPostCard25" class="button_left">
+				  <option value="" selected="selected">25 Card Value Pack Options: $50.00 ($2 per card)</option>
+				  <option value="1001 | PostCard25 | Utah">Utah Pack</option>
+				  <option value="1001 | PostCard25 | Colorado">ColoradoPack</option>
+				  <option value="1001 | PostCard25 | Sawtooths">Sawtooths Pack</option>
+				  <option value="1001 | PostCard25 | Glacier NP">Glacier NP Pack</option>
+				  <option value="1001 | PostCard25 | Southwest">Southwest Pack</option>
+				  <option value="1001 | PostCard25 | Pac NW">Pacific Northwest Pack</option>
+				  <option value="1001 | PostCard25 | Wyoming">Wyoming Pack</option>
+				  <option value="1001 | PostCard25 | Wildlife">Wildlife Pack</option>
+				  <option value="1001 | PostCard25 | Wildflower">Wildflower Pack</option>
+				  <option value="1001 | PostCard25 | Reflections">Reflections Pack</option>
+				  <option value="1001 | PostCard25 | Waterfall/Icefall">Waterfall and Icefall Pack</option>
+				  <option value="1001 | PostCard25 | Desert">Desert Pack</option>
+				  <option value="1001 | PostCard25 | Autumn/Winter">Autumn/Winter Pack</option>
+			  </select>&nbsp;
+				<input type="hidden" name="postcard25-submitted" value="yes" /> 
+				<input type="submit" name="submit-postcard25" value="Add to Cart" class="button_right"/>
+			</p>
+		  </form>
+		   
+	   <h3>50 Post Card Packs</h3>	
+	     <p>50 unique 4" X 6" Post Cards from the category of your choice.<br />
+	     I have fewer options on these listed, due to my no duplicates policy. These are $35, or $0.70 per card.<br />
+	     All cards unique. (No photo used more than once.)
+	     </p>	
+	     <p>For larger orders than I have listed here <a href="contact.php">contact me</a>. 
+	     </p>	  
+		 <form method="post" action="cart.php">
+			<p>
+			  <select name="BuyPostCard50" class="button_left">
+				  <option value="" selected="selected">50 Card Value Pack Options: $75.00 ($1.50 each)</option>
+				  <option value="1001 | PostCard50 | Utah">Utah Pack</option>
+				  <option value="1001 | PostCard50 | Rockies">Rocky Mountain Pack</option>
+				  <option value="1001 | PostCard50 | Southwest">Southwest Pack</option>
+				  <option value="1001 | PostCard50 | Pacific NW">Pacific NW Pack</option>
+				  <option value="1001 | PostCard50 | Wildflower">Wildflower Pack</option>
+				  <option value="1001 | PostCard50 | Autumn/Winter">Autumn/Winter Pack</option>
+				  <option value="1001 | PostCard50 | Desert">Desert Pack</option>
+			  </select>&nbsp;
+				<input type="hidden" name="postcard50-submitted" value="yes" /> 
+				<input type="submit" name="submit-postcard50" value="Add to Cart" class="button_right"/>
+			</p>
+		  </form>	  
+		  
     <h3>5" X 7" Flip Cards</h3>
       <img src="Pictures/CardsOnWall.jpg" alt="A variety of print sizes and styles hanging on a wall, including many flip cards."/>
       <p>Flip Cards are hand-cut, glued, and folded by yours truly, Andrew David. I use sturdy good quality card-stock in a variety of colors.<br />
-      Below are preset options for them packs. If you do not see exactly what you want, yes you can <a href="contact.php">contact me</a>.<br />
+      Below are preset options for theme packs. If you do not see exactly what you want, yes you can <a href="contact.php">contact me</a>.<br />
       Card packs come without duplicates, so if you order a 25 card pack, you will receive 25 unique cards with 25 different photos.
       Background colors will be varied, unless you message me before I complete your order with a special request, such as, "put all the cards on black".<br />
       I choose colors carefully to accent the particular photo, and you can trust my good eye and judgement, in my humble opinion.
       </p>
-      <p>I will likely offer post cards soon.</p>
       <img src="Pictures/CardsLayedOut.jpg" />
       <hr />
-      <h3>10 Card Packs</h3>
-        <p>10 Unique Cards from the theme of your choice. Lots of options. Packs are $25.00, for a price of $2.50 per card.</p>
+      <h3>10 Flip Card Packs</h3>
+        <p>10 Unique 5" X 7" Flip Cards from the theme of your choice. Lots of options. Packs are $25.00, for a price of $2.50 per card.<br />
+        All cards unique. (No photo used more than once.)<br />
+        Flip card packs will come in a variety of colors.
+        </p>
 	      <form method="post" action="cart.php">
 			<p>
 			  <select name="BuyCard10" class="button_left">
@@ -70,8 +165,11 @@
 				<input type="submit" name="submit-card10" value="Add to Cart" class="button_right"/>
 			</p>
 		  </form>
-		<h3>25 Card Packs</h3>  
-		   <p>25 unique cards from the category of your choice. These packs are $50 each, or $2 per card. Still many options.</p>
+		<h3>25 Flip Card Packs</h3>  
+		   <p>25 unique 5" X 7" Flip cards from the category of your choice. These packs are $50 each, or $2 per card.
+		   All cards unique. (No photo is used multiple times.)<br />
+		   Flip cards come in a variety of colors.
+		   </p>
 		   <form method="post" action="cart.php">
 			<p>
 			  <select name="BuyCard25" class="button_left">
@@ -95,11 +193,14 @@
 			</p>
 		  </form>
 		   
-	   <h3>50 Card Packs</h3>	
-	     <p>50 unique cards in the category of your choice.
-	     I have fewer options on these listed, due to my no duplicates policy. These are $75, or $1.50 per card.</p>	
-	     <p>For larger orders, or special instructions,<a href="contact.php">contact me</a> for now. I will improve this page to allow for further discounts and custom options in the future, but 
-	     that will get complicated and for now, an old-fashioned phone call or email makes more sense for my business.</p>	  
+	   <h3>50 Flip Card Packs</h3>	
+	     <p>50 unique 5" X 7" Flip Cards from the category of your choice.<br />
+	     I have fewer options on these listed, due to my no duplicates policy. These are $75, or $1.50 per card.<br />
+	     All cards unique. (No photo used more than once.)
+	     Flip cards come in a variety of colors.
+	     </p>	
+	     <p>For larger orders than I have listed here <a href="contact.php">contact me</a>. 
+	     </p>	  
 		 <form method="post" action="cart.php">
 			<p>
 			  <select name="BuyCard50" class="button_left">
